01995759d44d9563f200b1b0b316a78ecb9929c0
Zhi Yong Huang <huangdanny21@gmail.com>
2023-04-18 13:20:45 -0700

    First init

---
+++ TradeME/TradeMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeMEApp.swift
+//  TradeME
+//
+//  Created by Zhi Yong Huang on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TradeMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
