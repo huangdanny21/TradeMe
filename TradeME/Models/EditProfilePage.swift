b947fe8a620cd8a86617eb41884a1913d564fab2
Zhi Yong Huang <huangdanny21@gmail.com>
2023-04-24 18:34:16 -0700

    Added FSCard and EditProfilePage classes

---
+++ TradeME/Models/EditProfilePage.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfilePage.swift
+//  TradeME
+//
+//  Created by Zhi Yong Huang on 4/24/23.
+//
+
+import Foundation
--- TradeME/Modules/Sign up/AuthViewModel.swift
+++ TradeME/Modules/Sign up/AuthViewModel.swift
@@ -9,24 +9,24 @@ import Foundation
 import FirebaseAuth
 
 class AuthViewModel: ObservableObject {
-  @Published var email = ""
-  @Published var password = ""
-  @Published var error: AuthError? = nil
-
-  private var auth = Auth.auth()
-
-  func signUp() {
-    auth.createUser(withEmail: email, password: password) { authResult, error in
-      if let error = error {
-        self.error = AuthError(message: error.localizedDescription)
-      } else {
-        print("User created successfully")
-      }
+    @Published var email = ""
+    @Published var password = ""
+    @Published var error: AuthError? = nil
+    
+    private var auth = Auth.auth()
+    
+    func signUp() {
+        auth.createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.error = AuthError(message: error.localizedDescription)
+            } else {
+                print("User created successfully")
+            }
+        }
     }
-  }
 }
 
 struct AuthError: Error, Identifiable {
-  let id = UUID()
-  var message: String
+    let id = UUID()
+    var message: String
 }
